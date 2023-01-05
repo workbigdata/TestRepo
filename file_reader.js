22ce0798d1bc435a90f38e9a9709f018b6db6991
workbigdata <86465869+workbigdata@users.noreply.github.com>
2023-01-05 13:47:00 +0200

    update2

--- file_reader.js
+++ file_reader.js
@@ -1,1 +1,24 @@
-console.log("file rider file");
+const fileInput = document.querySelector('[type="file"]');
+const preview = document.getElementById('preview');
+
+fileInput.addEventListener('change', () => {
+// This function runs when file is selected
+
+    const fr = new FileReader(); 
+    // Creates new FileReader instance
+
+    fr.readAsDataURL(fileInput.files[0]);
+    // Reads user-selected file (other reading methods possible)
+    
+    fr.addEventListener('load', () => {
+        // This function runs when reading is complete
+        
+        const url = fr.result;
+        // Result always available on result property on the file reader
+
+        console.log(url); 
+        // data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA14AAAOaCA... (very long!)
+        
+    })
+
+});
